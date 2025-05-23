ffb61dc27260a580fdf763189a0d6cdc3783ac8e
신호현 <black5824@naver.com>
2025-05-23 14:39:12 +0900

    서버 요청 추가

---
+++ lib/model/course.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Course {
+  final int course_id;
+  final String title;
+  final String distance;
+  final String imagePath;
+  final String location;
+  final List<String> tags;
+
+  Course({
+    required this.course_id,
+    required this.title,
+    required this.distance,
+    required this.location,
+    required this.imagePath,
+    required this.tags,
+  });
+
+  factory Course.fromJson(Map<String, dynamic> json) {
+    return Course(
+      course_id: json['course_id'],
+      title: json['title'],
+      distance: json['distance'],
+      location: json['location'],
+      imagePath: json['image_url'],
+      tags: List<String>.from(json['tags'] ?? []),
+    );
+  }
+}
--- lib/screen/course_recommended_screen.dart
+++ lib/screen/course_recommended_screen.dart
@@ -1,29 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:prunners/widget/bottom_bar.dart';
-import 'package:prunners/widget/top_bar.dart';
-
-class Course {
-  final int id;
-  final String title;
-  final String distance;
-  final String imageUrl;
-
-  Course({
-    required this.id,
-    required this.title,
-    required this.distance,
-    required this.imageUrl,
-  });
-
-  factory Course.fromJson(Map<String, dynamic> json) {
-    return Course(
-      id: json['id'],
-      title: json['title'],
-      distance: json['distance'],
-      imageUrl: json['image_url'],
-    );
-  }
-}
+import 'package:prunners/model/course.dart';
+import 'course_screen.dart';
+import 'package:prunners/model/course_service.dart';
 
 class CourseRecommendedScreen extends StatefulWidget {
   const CourseRecommendedScreen({super.key});
@@ -33,59 +12,118 @@ class CourseRecommendedScreen extends StatefulWidget {
 }
 
 class _CourseRecommendedScreenState extends State<CourseRecommendedScreen> {
+  // MockData
+  /*
+  final List<Course> nearbyCourses = [
+    Course(course_id: 1, title: '러닝 코스 1', distance: '3.5 km', location: '서울',
+      imagePath: 'assets/111.png', tags: ['#한강', '#초보자용', '#야경좋음'],),
+    Course(course_id: 2, title: '러닝 코스 2', distance: '3.5 km', location: '서울',
+      imagePath: 'assets/111.png', tags: ['#한강', '#초보자용', '#야경좋음'],),
+    Course(course_id: 3, title: '러닝 코스 3', distance: '5 km', location: '서울',
+      imagePath: 'assets/111.png', tags: ['#한강', '#초보자용', '#야경좋음'],),
+  ];
+
+  final List<Course> popularCourses = [
+    Course(course_id: 6, title: '러닝 코스 4', distance: '5 km', location: '서울',
+      imagePath: 'assets/111.png', tags: ['#한강', '#초보자용', '#야경좋음'],),
+    Course(course_id: 7, title: '러닝 코스 5', distance: '5 km', location: '서울',
+      imagePath: 'assets/111.png', tags: ['#한강', '#초보자용', '#야경좋음'],),
+    Course(course_id: 8, title: '러닝 코스 6', distance: '5 km', location: '서울',
+      imagePath: 'assets/111.png', tags: ['#한강', '#초보자용', '#야경좋음'],),
+    Course(course_id: 9, title: '러닝 코스 7', distance: '5 km', location: '서울',
+      imagePath: 'assets/111.png', tags: ['#한강', '#초보자용', '#야경좋음'],),
+    Course(course_id: 10, title: '러닝 코스 8', distance: '5 km', location: '서울',
+      imagePath: 'assets/111.png', tags: ['#한강', '#초보자용', '#야경좋음'],),
+  ];
+  */
+
+  final CourseService _courseService = CourseService();
+
+  late Future<List<Course>> _nearbyCourse;
+  late Future<List<Course>> _popularCourse;
+
+  @override
+  void initState() {
+    super.initState();
+    _nearbyCourse = _courseService.getNearbyCourse();
+    _popularCourse = _courseService.getPopularCourse();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(60),
-        child: CustomTopBar(title: '코스 추천'),
+      appBar: AppBar(
+        title: const Text(
+          '코스 추천',
+          style: TextStyle(
+            fontWeight: FontWeight.w600,
+            fontSize: 25,
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
       ),
       body: SingleChildScrollView(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text('내 주변 러닝 코스', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-              const SizedBox(height: 20),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    CourseCard(title: '러닝 코스 1', distance: '3.5 km', imagePath: 'assets/111.png'),
-                    SizedBox(width: 12),
-                    CourseCard(title: '러닝 코스 2', distance: '3.5 km', imagePath: 'assets/111.png'),
-                    SizedBox(width: 12),
-                    CourseCard(title: '러닝 코스 3', distance: '5 km', imagePath: 'assets/111.png'),
-                    SizedBox(width: 12),
-                    CourseCard(title: '러닝 코스 4', distance: '5 km', imagePath: 'assets/111.png'),
-                    SizedBox(width: 12),
-                    CourseCard(title: '러닝 코스 5', distance: '5 km', imagePath: 'assets/111.png'),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 50),
-              const Text('이번주 인기 러닝 코스', style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
-              const SizedBox(height: 20),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    CourseCard(title: '러닝 코스 6', distance: '5 km', imagePath: 'assets/111.png'),
-                    SizedBox(width: 12),
-                    CourseCard(title: '러닝 코스 7', distance: '5 km', imagePath: 'assets/111.png'),
-                    SizedBox(width: 12),
-                    CourseCard(title: '러닝 코스 8', distance: '5 km', imagePath: 'assets/111.png'),
-                    SizedBox(width: 12),
-                    CourseCard(title: '러닝 코스 9', distance: '5 km', imagePath: 'assets/111.png'),
-                    SizedBox(width: 12),
-                    CourseCard(title: '러닝 코스 10', distance: '5 km', imagePath: 'assets/111.png'),
-                    SizedBox(width: 12),
-                    CourseCard(title: '러닝 코스 11', distance: '5 km', imagePath: 'assets/111.png'),
-                  ],
-                ),
-              ),
-            ],
-          )
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text('내 주변 러닝 코스', style: TextStyle(fontSize: 23, fontWeight: FontWeight.w600)),
+            const SizedBox(height: 20),
+            FutureBuilder<List<Course>>(
+              future: _nearbyCourse,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: snapshot.data!
+                        .map((course) => Padding(
+                      padding: const EdgeInsets.only(right: 12.0),
+                      child: CourseCard(course: course),
+                    )).toList(),
+                  ),
+                );
+              },
+            ),
+            const SizedBox(height: 50),
+            const Text('이번주 인기 러닝 코스', style: TextStyle(fontSize: 23, fontWeight: FontWeight.w600)),
+            const SizedBox(height: 20),
+            FutureBuilder<List<Course>>(
+              future: _popularCourse,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: snapshot.data!
+                        .map((course) => Padding(
+                      padding: const EdgeInsets.only(right: 12.0),
+                      child: CourseCard(course: course),
+                    ))
+                        .toList(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: SafeArea(
         top: false,
@@ -104,22 +142,23 @@ class _CourseRecommendedScreenState extends State<CourseRecommendedScreen> {
 }
 
 class CourseCard extends StatelessWidget {
-  final String title;
-  final String distance;
-  final String imagePath;
+  final Course course;
 
   const CourseCard({
     super.key,
-    required this.title,
-    required this.distance,
-    required this.imagePath,
+    required this.course,
   });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.pushNamed(context, '/course_detail');
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => CourseScreen(course: course),
+          ),
+        );
       },
       child: Column(
         children: [
@@ -129,14 +168,14 @@ class CourseCard extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               image: DecorationImage(
-                image: AssetImage(imagePath),
+                image: AssetImage(course.imagePath),
                 fit: BoxFit.cover,
               ),
             ),
           ),
           const SizedBox(height: 8),
-          Text(title, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
-          Text(distance, style: const TextStyle(fontSize: 16)),
+          Text(course.title, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
+          Text(course.distance, style: const TextStyle(fontSize: 16)),
         ],
       ),
     );
